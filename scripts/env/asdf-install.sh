b4865e4a5a7bb34f9ba071bce43ad98b5f67987c
Nick DeRobertis <whoopnip@gmail.com>
2024-08-02 16:31:56 -0600

    chore: Install go and terraform with asdf

---
+++ scripts/env/asdf-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Note: Must be run through direnv source
+
+if ! has asdf; then
+  echo "asdf not found. Please install asdf first: https://asdf-vm.com/guide/getting-started.html"
+  exit 1
+fi
+
+# Install asdf plugins
+plugin_list=$(asdf plugin-list)
+plugins=("golang" "terraform")
+
+for plugin in "${plugins[@]}"; do
+  if ! echo $plugin_list | grep -q " $plugin "; then
+    asdf plugin-add $plugin
+    echo "Added asdf plugin $plugin"
+  fi
+done
+
+# Install necessary versions
+asdf install
